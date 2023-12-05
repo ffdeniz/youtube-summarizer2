fe590e5cee049b9d60dfa3d7c78e382350a8fa9e
Deniz <deniz.ozgoren@freeflysystems.com>
2023-12-04 22:26:54 -0800

    WIP - youtube summarizer

--- api/get-youtube-summary.ts
+++ api/get-youtube-summary.ts
@@ -1,17 +1,29 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { YoutubeTranscript } from 'youtube-transcript';
+import { YoutubeTranscript } from "youtube-transcript";
 
 // Explicitly set Serverless Function on the Node.js runtim
 // export const config = {
 //   runtime: "nodejs",
 // };
 
-export default function (request: VercelRequest, response: VercelResponse) {
-  if (request.method === 'POST') {
+
+export default async function (request: VercelRequest, response: VercelResponse) {
+  if (request.method === "POST") {
     const { videoUrl } = request.body;
-    console.log(videoUrl);
+
+    // Get the video ID from the URL
+    const videoId = String(new URL(videoUrl).searchParams.get('v'));
+ 
+    // Get the transcript from the video
+    const transcript_list = await YoutubeTranscript.fetchTranscript(videoId);
+    const transcript_text = transcript_list.map(item => item['text']).join(' ');
+    console.log(transcript_text);
+
+    console.log("OpenAI API Key: " + process.env.OPENAI_API_KEY);
     response.send({ transcript: "lorem ipsum" });
   } else {
-    response.send('Method not allowed');
+    response.send("Method not allowed");
   }
 }
+
+
